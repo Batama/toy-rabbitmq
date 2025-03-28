5d2cb8992c012521039e1264f66210291d1b839a
Batama <thebatama@gmail.com>
2025-03-28 12:00:15 +0900

    add volumes to container

--- step1/startRabbitMQ.sh
+++ step1/startRabbitMQ.sh
@@ -1,6 +1,9 @@
 docker run -d \
   --hostname demo-rabbit \
   --name demo-rabbit \
+  -v rabbitmq_data:/var/lib/rabbitmq \
+  -v rabbitmq_logs:/var/log/rabbitmq \
+  -v rabbitmq_config:/etc/rabbitmq \
   -p 5672:5672 \
   -p 15672:15672 \
  rabbitmq:4-management
